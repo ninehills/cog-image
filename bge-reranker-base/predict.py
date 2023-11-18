37e4cb8878fdc250f1ecc7bb7e055877dfeb3623
Tao Yang <swulling@gmail.com>
2023-11-18 21:39:44 +0800

    init

---
+++ bge-reranker-base/predict.py
@@ -0,0 +1,33 @@
+import torch
+import json
+import time
+
+from cog import BasePredictor, Input, BaseModel
+from typing import List
+from FlagEmbedding import FlagReranker
+
+class Output(BaseModel):
+    """Output schema for the predictor"""
+    scores: List[float]
+    model_name: str
+    use_fp16: bool
+
+
+class Predictor(BasePredictor):
+    def setup(self):
+        """Load the model into memory to make running multiple predictions efficient"""
+        self.model_name = "bge-reranker-base"
+        self.use_fp16 = torch.cuda.is_available()
+        self.model = FlagReranker(f"checkpoints", use_fp16=self.use_fp16)
+
+    def predict(self,
+        pairs_json: str = Input(description='Input pairs, eg: [["a", "b"], ["c", "d"]]'),
+    ) -> Output:
+        """Run a single prediction on the model"""
+        inputs = json.loads(pairs_json)
+        all_scores = self.model.compute_score(inputs)
+        if isinstance(all_scores, list):
+            scores = all_scores
+        else:
+            scores = [all_scores]
+        return Output(scores=scores, model_name=self.model_name, use_fp16=self.use_fp16)
